12c47f27287a3956fd83ee72b285410a2e33f6bd
Zhouyex <591742716@qq.com>
2021-07-15 14:26:03 +0800

    lab-test

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -290,7 +290,7 @@ export default function Swap() {
 
           <AutoColumn gap={'md'}>
             <CurrencyInputPanel
-              label={independentField === Field.OUTPUT && !showWrap && trade ? 'From (estimated)' : 'from'}
+              label={independentField === Field.OUTPUT && !showWrap && trade ? 'From (estimated)' : 'FROM'}
               value={formattedAmounts[Field.INPUT]}
               showMaxButton={!atMaxAmountInput}
               currency={currencies[Field.INPUT]}
@@ -322,7 +322,7 @@ export default function Swap() {
             <CurrencyInputPanel
               value={formattedAmounts[Field.OUTPUT]}
               onUserInput={handleTypeOutput}
-              label={independentField === Field.INPUT && !showWrap && trade ? 'To (estimated)' : 'to'}
+              label={independentField === Field.INPUT && !showWrap && trade ? 'To (estimated)' : 'TO'}
               showMaxButton={false}
               currency={currencies[Field.OUTPUT]}
               onCurrencySelect={handleOutputSelect}
